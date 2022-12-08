6742f1a833ced9895d47ef7162fbde35b7b7a7d8
Peter Wong <peter.wm.wong@gmail.com>
2022-12-08 14:55:58 -0600

    WIP: Attempt to generate a Metal GPU Binary Archive (see build.sh)
    As of macOS 13.1 RC (22C65) / Xcode 14.2 RC (14C18)...
    - Good News
        - metal-source is fixed and correctly generates a render pipeline script
        - metal can use the render pipeline script to generate a GPU Binary Archive
    - Bad News
        - Cannot get Metal to use the GPU Binary Archive
        - Metal makeRenderPipelineState (with .failOnBinaryArchiveMiss) complains it cannot find main_fragment
        - It "works" if metallib includes BOTH native and air (see last few lines of build.sh)
        - Unfortunately, when profiling w/Instruments the "Metal Compiler" lane shows it's **STILL** compiling the damn shaders (VS/FS)
        - ^^^ Becareful, Metal has caches somewhere, if you rerun it won't compile. You need to change the shader code, run build.sh and run to avoid the cache.

--- x-metal-source-on-harvested-gpu-archive/main.swift
+++ x-metal-source-on-harvested-gpu-archive/main.swift
@@ -2,10 +2,12 @@ import Foundation
 import Metal
 import MetalKit
 
+let IS_GENERATING_PIPELINE_SCRIPT = false
+
 func shell(_ command: String) throws {
     let task = Process()
     let pipe = Pipe()
-
+    
     print("\nCommand: \(command)");
     task.standardOutput = pipe
     task.standardError = pipe
@@ -17,14 +19,16 @@ func shell(_ command: String) throws {
     print("Command stdout/stderr:\n\(result)");
 }
 
-print("""
+if IS_GENERATING_PIPELINE_SCRIPT {
+    print("""
 
 -----------
 Environment
 -----------
 """)
-try shell("xcrun --show-sdk-path")
-try shell("xcrun xcodebuild -version")
+    try shell("xcrun --show-sdk-path")
+    try shell("xcrun xcodebuild -version")
+}
 
 
 print("""
@@ -35,50 +39,116 @@ Creating Render Pipeline
 """)
 let device = MTLCreateSystemDefaultDevice()!
 /* See shaders.metal */
-let lib = device.makeDefaultLibrary()!
+var lib;
+if IS_GENERATING_PIPELINE_SCRIPT {
+    lib = device.makeDefaultLibrary()!
+} else {
+    lib = try! device.makeLibrary(URL: URL(filePath: "/Users/pwong/projects/x-metal-source-on-harvested-gpu-archive/x-metal-source-on-harvested-gpu-archive/shaders-applegpu_13g.metallib"))
+}
 let pipelineDesc = MTLRenderPipelineDescriptor()
+if !IS_GENERATING_PIPELINE_SCRIPT {
+    let desc = MTLBinaryArchiveDescriptor()
+    desc.url = URL(filePath: "/Users/pwong/projects/x-metal-source-on-harvested-gpu-archive/x-metal-source-on-harvested-gpu-archive/shaders-applegpu_13g.metallib")
+    let bin = try! device.makeBinaryArchive(descriptor: desc)
+    pipelineDesc.binaryArchives = [bin]
+}
 pipelineDesc.vertexFunction = lib.makeFunction(name: "main_vertex")
 pipelineDesc.fragmentFunction = lib.makeFunction(name: "main_fragment")
-pipelineDesc.colorAttachments[0]?.pixelFormat = .bgra8Unorm
+pipelineDesc.colorAttachments[0]?.pixelFormat = .rgba8Unorm
 
-print("""
+
+if !IS_GENERATING_PIPELINE_SCRIPT {
+    let WIDTH = 4
+    let HEIGHT = 4
+    
+    // let pipeline = try! device.makeRenderPipelineState(descriptor: pipelineDesc, options: .failOnBinaryArchiveMiss).0
+    let pipeline = try! device.makeRenderPipelineState(descriptor: pipelineDesc)
+    
+    let textureBuf = device.makeBuffer(length: 4 /* 4-bytes (rgba) */ * WIDTH * HEIGHT)!
+    let textureDesc = MTLTextureDescriptor()
+    textureDesc.storageMode = .shared
+    textureDesc.depth = 1
+    textureDesc.pixelFormat = .rgba8Unorm
+    textureDesc.usage = .renderTarget
+    textureDesc.width = WIDTH
+    textureDesc.height = HEIGHT
+    let output = textureBuf.makeTexture(descriptor: textureDesc, offset: 0, bytesPerRow: 4 * WIDTH)
+    
+    let queue = device.makeCommandQueue()!
+    let command = queue.makeCommandBuffer()!
+    
+    let renderDesc = MTLRenderPassDescriptor()
+    let color = renderDesc.colorAttachments[0]!
+    color.texture = output
+    color.loadAction = .clear
+    color.storeAction = .store
+    color.clearColor = MTLClearColorMake(0, 0, 0, 0)
+    
+    let r = command.makeRenderCommandEncoder(descriptor: renderDesc)!
+    r.setRenderPipelineState(pipeline)
+    r.drawPrimitives(type: .point, vertexStart: 0, vertexCount: 1)
+    r.endEncoding()
+    
+    command.commit()
+    command.waitUntilCompleted()
+    
+    let outputPixels = Array(textureBuf.contents().withMemoryRebound(to: (UInt8, UInt8, UInt8, UInt8).self, capacity: WIDTH * HEIGHT) {
+        UnsafeBufferPointer(start: $0, count: WIDTH * HEIGHT)
+    })
+    
+    func printOutputPixels(_ arr: Array<(UInt8, UInt8, UInt8, UInt8)>) {
+        for y in 0..<HEIGHT {
+            var r: [UInt8] = []
+            for x in 0..<WIDTH {
+                r.append(arr[y * WIDTH + x].1)
+            }
+            print(r)
+        }
+        
+    }
+    printOutputPixels(outputPixels)
+}
+
+if IS_GENERATING_PIPELINE_SCRIPT {
+    print("""
 
 ----------------------
 Harvesting GPU Archive
 ----------------------
 
 """)
-let archivePath = NSTemporaryDirectory().appending("harvested-archive.metallib")
-let archiveDesc = MTLBinaryArchiveDescriptor()
-let archive = try device.makeBinaryArchive(descriptor: archiveDesc)
-try archive.addRenderPipelineFunctions(descriptor: pipelineDesc)
-try archive.serialize(to: NSURL.fileURL(withPath: archivePath))
-print("Created archive: \(archivePath)")
-
-print("""
+    let archivePath = NSTemporaryDirectory().appending("harvested-archive.metallib")
+    let archiveDesc = MTLBinaryArchiveDescriptor()
+    let archive = try device.makeBinaryArchive(descriptor: archiveDesc)
+    try archive.addRenderPipelineFunctions(descriptor: pipelineDesc)
+    try archive.serialize(to: NSURL.fileURL(withPath: archivePath))
+    print("Created archive: \(archivePath)")
+    
+    print("""
 
 --------------------------------------------
 Verify/Display Information about GPU Archive
 --------------------------------------------
 """)
-
-try shell("xcrun metal-readobj \(archivePath)")
-
-print("""
+    
+    try shell("xcrun metal-readobj \(archivePath)")
+    
+    print("""
 
 ---------------------------------------------
 Using metal-source to get pipeline descriptor
 ---------------------------------------------
 """)
-let descriptorsPath = NSTemporaryDirectory().appending("descriptors.mtlp-json")
-
-try shell("rm -rf \(descriptorsPath) # Remove any existing")
-try shell("xcrun metal-source -flatbuffers=json \(archivePath) -o \(descriptorsPath)")
-
-print("""
+    let descriptorsPath = NSTemporaryDirectory().appending("descriptors.mtlp-json")
+    
+    try shell("rm -rf \(descriptorsPath) # Remove any existing")
+    try shell("xcrun metal-source -flatbuffers=json \(archivePath) -o \(descriptorsPath)")
+    
+    print("""
 
 -------------------------------------------------
 Display descriptor... directory? not a JSON file?
 -------------------------------------------------
 """)
-try shell("find \(descriptorsPath)")
+    try shell("find \(descriptorsPath)")
+}
